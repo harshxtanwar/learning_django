4caeb5f24706891243a69eb3ee015f3d4be52a68
harshxtanwar <harshwardhan.t12345@gmail.com>
2022-08-16 20:31:58 +0530

    making a project now for meetups

--- mysite/meetups/views.py
+++ mysite/meetups/views.py
@@ -4,7 +4,36 @@ from django.http import HttpResponse
 
 
 def index (request):
-    return render(request, 'meetups/index.html')
+
+    # we defined our own temporary dataset
+    meetups = [
+        {
+        'title': 'A First Meetup',
+        'location': 'New York',
+        'slug': 'a-first-meetup'
+        },
+        {
+        'title': 'A Second Meetup',
+        'location': 'Chicago',
+        'slug': 'a-second-meetup'
+        },
+        {
+        'title': 'A Third Meetup',
+        'location': 'Dubai', 'slug': 'a-third-meetup'
+        },
+        {
+        'title': 'A Fourth Meetup',
+        'location': 'Paris',
+        'slug': 'a-fourth-meetup'
+        }
+
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups' : True,
+        # we define a key to the database we made in a list
+        # the key is meetups_key in our case
+        'meetups_key': meetups
+    })
 
 def greetings(request):
     return HttpResponse('namaste')
